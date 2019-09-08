080bdba4f28df5a5c9a58bd30868bb2f1b000169
Austin Salisbury <austinsa1423@gmail.com>
2019-09-08 09:39:09 -0400

    adding class files for cis 443

---
+++ Tutorials/Console Hello/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Console_Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string pause = Console.ReadLine();
+        }
+    }
+}
